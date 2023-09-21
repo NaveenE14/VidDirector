afe298821384547f044118ef0e7c8a11608ad613
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-09-21 12:19:56 +0530

    Update appe.py

--- appe.py
+++ appe.py
@@ -35,7 +35,7 @@ if st.button("Generate Video"):
 
     Remember to format the output as follows:
 
-    Image: Insert image description or scene here aspect:{video_aspect}
+    Image: Insert image description or scene here aspect:{video_aspect} style: {video_style}
     Narrator: Write the narration or dialogue for the sentence 
 
     Image: Insert image description or scene here aspect:{video_aspect}  
@@ -43,9 +43,10 @@ if st.button("Generate Video"):
 
     Image: Insert image description or scene here aspect:{video_aspect}
     Narrator: Write the narration or dialogue for the sentence 
+
     And so on until the end of the story.
+
     Return without commentary. Only the narrator should speak no other characters in the story. Give highly detailed 2 lines image prompts that relate to previous scenes. The prompts should be similar, and the images generated should be similar to each other because I am going to create a video using that."""
-    
     from dotenv import load_dotenv
     load_dotenv()
     clarifai_pat = os.getenv('CLARIFAI_PAT')
@@ -60,9 +61,8 @@ if st.button("Generate Video"):
     userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
     post_model_outputs_response = stub.PostModelOutputs(
         service_pb2.PostModelOutputsRequest(
-            user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
+            user_app_id=userDataObject, 
             model_id=MODEL_ID,
-            
             inputs=[
                 resources_pb2.Input(
                     data=resources_pb2.Data(
@@ -117,22 +117,122 @@ if st.button("Generate Video"):
     st.success("Scripts Generated Sucessfully")
     os.makedirs("audio", exist_ok=True)
 
-
-    st.success("Generating audio files...")
+    
+    st.success("Generating audio files using Eleven Labs speech synthesis model")
    
-
-# Iterate through narrator prompts and convert to audio
+    USER_ID = 'eleven-labs'
+    APP_ID = 'audio-generation'
+    MODEL_ID = 'speech-synthesis'
+    if not os.path.exists("audio"):
+        os.makedirs("audio")
     for idx, narrator_prompt in enumerate(narrator_prompts):
-        # Convert text to audio
-        tts = gTTS(text=narrator_prompt, lang='en', slow=False)
-        
-        # Define the audio filename with a unique index
-        audio_filename = f"audio/narrator_audio{idx}.mp3"
-        
-        # Save the audio file
-        tts.save(audio_filename)
-        print(f"Saved audio {idx}: {audio_filename}")
-        
+        userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+        post_model_outputs_response = stub.PostModelOutputs(
+            service_pb2.PostModelOutputsRequest(
+                user_app_id=userDataObject,
+                model_id=MODEL_ID,
+                inputs=[
+                    resources_pb2.Input(
+                        data=resources_pb2.Data(
+                            text=resources_pb2.Text(
+                                raw=narrator_prompt
+                            )
+                        )
+                    )
+                ]
+            ),
+            metadata=metadata
+        )
+        if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+            print(post_model_outputs_response.status)
+            raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+        output = post_model_outputs_response.outputs[0]
+
+        print(f"{idx+1}Audio Generated")
+        for concept in output.data.concepts:
+            print("%s %.2f" % (concept.name, concept.value))
+
+        base64_audio = output.data.audio.base64
+        audio_filename = f"audio/audio_{idx}.mp3"  # Save audio in the "audio" folder with the .mp3 extension
+        with open(audio_filename, 'wb') as f:
+            f.write(base64_audio)
+
     st.success("Audio files Generated Sucessfully")
 
+    st.success("Generating images using Stable Diffusion XL model")
+    USER_ID = 'stability-ai'
+    APP_ID = 'stable-diffusion-2'
+    MODEL_ID = 'stable-diffusion-xl'
+    if not os.path.exists("image"):
+        os.makedirs("image")
+    for idx, image_prompt in enumerate(image_prompts):
+        userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+        post_model_outputs_response = stub.PostModelOutputs(
+            service_pb2.PostModelOutputsRequest(
+                user_app_id=userDataObject,
+                model_id=MODEL_ID,
+                inputs=[
+                    resources_pb2.Input(
+                        data=resources_pb2.Data(
+                            text=resources_pb2.Text(
+                                raw=image_prompt
+                            )
+                        )
+                    )
+                ]
+            ),
+            metadata=metadata
+        )
+        if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+            print(post_model_outputs_response.status)
+            raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+        output = post_model_outputs_response.outputs[0]
+        print(f"{idx+1}image generated")
+        for concept in output.data.concepts:
+            print("%s %.2f" % (concept.name, concept.value))
+
+        base64_image = output.data.image.base64
+        image_filename = f"image/image_{idx}.jpg"   
+        with open(image_filename, 'wb') as f:
+            f.write(base64_image)
     
+    st.success("Images Generated Sucessfully")
+    st.success("Generating video using moviepy")
+    from moviepy.editor import ImageClip, concatenate_videoclips
+    from moviepy.audio.io.AudioFileClip import AudioFileClip
+    output_video_filename = "output_video.mp4"
+    clips = []
+    for idx, image_prompt in enumerate(image_prompts):
+        image_filename = f"image/image_{idx}.jpg"
+        audio_filename = f"audio/audio_{idx}.mp3"
+        image_clip = ImageClip(image_filename)
+        audio_clip = AudioFileClip(audio_filename)
+        audio_clip = audio_clip.set_duration(image_clip.duration)
+        clip = image_clip.set_audio(audio_clip)
+        clips.append(clip)
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(output_video_filename)
+    st.success("Video Generated Sucessfully")
+    st.ballons()
+    st.video(output_video_filename)
+    #add subtitles in the video using narrator_prompts  
+    st.success("Adding subtitles to the video")
+    import pysrt
+    subs = pysrt.SubRipFile()
+    for idx, narrator_prompt in enumerate(narrator_prompts):
+        subs.append(pysrt.SubRipItem(
+            index=idx+1,
+            start=clips[idx].start,
+            end=clips[idx].end,
+            text=narrator_prompt
+        ))
+    subs.save("output_video.srt")
+    st.success("Subtitles added Sucessfully")
+    st.success("Video Generated Sucessfully")
+    st.ballons()
+    st.video("output_video.mp4")
+    
+
+    
+
+            
